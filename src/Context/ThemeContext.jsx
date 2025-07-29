90cdaaeb5441b3f80a8ca659c62807a9801763b9
NabilaAwad <105739139+NabilaAwad@users.noreply.github.com>
2025-07-29 10:43:34 +0300

    Update ThemeContext.jsx

--- src/Context/ThemeContext.jsx
+++ src/Context/ThemeContext.jsx
@@ -1,4 +1,4 @@
-// src/Context/ThemeContext.jsx
+
 import React, { createContext, useContext, useState, useEffect } from 'react'
 
 const ThemeContext = createContext()
@@ -9,9 +9,9 @@ export const ThemeProvider = ({ children }) => {
   const [themeName, setThemeName] = useState('light')
 
   useEffect(() => {
-    // إزالة أي ثيم سابق
+
     themes.forEach(theme => document.documentElement.classList.remove(theme))
-    // أضف الثيم الحالي
+
     document.documentElement.classList.add(themeName)
   }, [themeName])
 
